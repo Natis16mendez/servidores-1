89c70407053d28edc21ca7931dcdfbdbe4019f26
mpnatali20@gmail.com <mpnatali20@gmail.com>
2024-08-27 16:01:51 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template 
+
+servidor1 = Flask(__name__)
+
+@servidor1.route("/inicio")
+def index ():
+    return render_template("index.html")
+if __name__=="main_":
+    servidor1.run(debug=True)
